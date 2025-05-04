1b295b65753e90e13e18218b7521f0330cd37077
khayitovdev <webdewoloper@gmail.com>
2025-05-04 11:34:42 +0500

    update

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 ALLOWED_HOSTS = ["195.158.6.58", "127.0.0.1", "localhost",  "umf.madami.uz", "www.umf.madami.uz",]
-DEBUG = False
+DEBUG = True
 
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
