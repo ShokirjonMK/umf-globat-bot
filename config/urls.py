1b05f88d600394efef712c4a468f2f6e378baa69
khayitovdev <webdewoloper@gmail.com>
2025-05-04 11:45:22 +0500

    update

--- config/urls.py
+++ config/urls.py
@@ -1,15 +1,15 @@
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from truck.views import TelegramWebhookView
 
 urlpatterns = [
     path('', admin.site.urls, name="truck_admin"),
     path('bot/', include('truck.urls'), name="truck"),
+    path('bot/webhook/', TelegramWebhookView.as_view(), name="telegram_webhook"), 
 ]
-
-
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- truck/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-from .views import TelegramWebhookView
-
-urlpatterns = [
-    path('webhook/', TelegramWebhookView.as_view(), name="telegram_webhook"),
-]
